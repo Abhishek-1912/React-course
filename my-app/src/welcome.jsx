f0755cb153401c70d363ed50b3c16d2204d4900c
Abhishek <abhi.monu0504@gmail.com>
2025-08-05 08:53:36 +0530

    Intial application setup with react

---
+++ my-app/src/welcome.jsx
@@ -0,0 +1,12 @@
+const Welcome = () => {
+    let name = "Abhishek"
+    return (
+      <div>
+        <h1>My name</h1>
+        <p>{name}</p>
+
+        </div>
+          )
+};
+
+export default Welcome
